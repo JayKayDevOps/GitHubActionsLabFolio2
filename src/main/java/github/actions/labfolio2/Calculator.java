3c73c7dcb4da539f5517da450cf7ea70a298325b
“jason.kelly” <L00194486@atu.ie>
2024-11-27 20:56:02 +0000

    1st Commit to GitHubActionsLabFolio2: Adding project to main

---
+++ src/main/java/github/actions/labfolio2/Calculator.java
@@ -0,0 +1,7 @@
+package github.actions.labfolio2;
+
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/github/actions/labfolio2/CalculatorTests.java
@@ -0,0 +1,18 @@
+package github.actions.labfolio2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTests {
+
+
+    @Test
+    void testCalulatorAdds(){
+        var calculator = new Calculator();
+        int actual = calculator.add(2,3);
+
+        assertEquals(5,actual);
+
+    }
+}
