36294612139258223a3234bf3657474449caf256
“jason.kelly” <L00194486@atu.ie>
2024-11-30 16:01:03 +0000

    7th Commit GitHubActionsLabFolio2: Adding Division and Adding Dependency to Pom file

--- src/main/java/github/actions/labfolio2/Calculator.java
+++ src/main/java/github/actions/labfolio2/Calculator.java
@@ -1,13 +1,16 @@
 package github.actions.labfolio2;
 
 public class Calculator {
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
     }
 
-
-    public int multiply(int a, int b){
+    public int multiply(int a, int b) {
         return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
     }
 }
 
--- src/test/java/github/actions/labfolio2/CalculatorTests.java
+++ src/test/java/github/actions/labfolio2/CalculatorTests.java
@@ -1,24 +1,37 @@
 package github.actions.labfolio2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+//import org.junit.runner.RunWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTests {
 
-
+class CalculatorTest {
     @Test
-    void testCalculatorAdds(){
+    void testCalculatorAdds() {
         var calculator = new Calculator();
-        int actual = calculator.add(2,3);
+        int actual = calculator.add(2, 3);
 
-        assertEquals(5,actual);
+        assertEquals(5, actual);
     }
+}
 
-    @Test
-    void testCalculatorMultiplies(){
+@Suite
+@SuiteDisplayName("A Calculator Test Suite")
+class TwoCalculatorTests {
+    void testCalculatorMultiplies() {
         var calculator = new Calculator();
         int actual = calculator.multiply(2, 3);
+
+        assertEquals(6, actual);
+
+    }
+
+    void testCalculatorDivides() {
+        var calculator = new Calculator();
+        int actual = calculator.divide(6, 3);
 
         assertEquals(6, actual);
 
